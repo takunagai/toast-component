4a39f33d643a116a786ec6a0ecb8bf53a448e844
takna <tak_na@icloud.com>
2023-07-01 09:58:55 +0900

    Fix: Toast style.

--- src/components/Toast/Toast.js
+++ src/components/Toast/Toast.js
@@ -19,10 +19,12 @@ const ICONS_BY_VARIANT = {
 };
 
 function Toast({ variant, children }) {
+  const Icon = ICONS_BY_VARIANT[variant];
+
   return (
-    <div className={`${styles.toast} ${styles.notice}`}>
+    <div className={`${styles.toast} ${styles[variant]}`}>
       <div className={styles.iconContainer}>
-        <Info size={24} />
+        <Icon size={24} />
       </div>
       <p className={styles.content}>
         {children}
